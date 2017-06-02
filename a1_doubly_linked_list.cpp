2af18f6c51dc9699e765713a214ac814d8a318f7
Emily Zeng <zhixuan.zeng@gmail.com>
2017-06-02 16:39:44 -0400

    init

---
+++ a1_doubly_linked_list.cpp
@@ -0,0 +1,93 @@
+#include "a1_doubly_linked_list.hpp"
+
+DoublyLinkedList::Node::Node(DataType data)
+{
+}
+
+DoublyLinkedList::DoublyLinkedList()
+{
+}
+
+DoublyLinkedList::~DoublyLinkedList()
+{
+}
+
+unsigned int DoublyLinkedList::size() const
+{
+}
+
+unsigned int DoublyLinkedList::capacity() const
+{
+}
+
+bool DoublyLinkedList::empty() const
+{
+}
+
+bool DoublyLinkedList::full() const
+{
+}
+
+DoublyLinkedList::DataType DoublyLinkedList::select(unsigned int index) const
+{
+}
+
+unsigned int DoublyLinkedList::search(DataType value) const
+{
+}
+
+void DoublyLinkedList::print() const
+{
+}
+
+DoublyLinkedList::Node* DoublyLinkedList::getNode(unsigned int index) const
+{
+}
+
+bool DoublyLinkedList::insert(DataType value, unsigned int index)
+{
+}
+
+bool DoublyLinkedList::insert_front(DataType value)
+{
+}
+
+bool DoublyLinkedList::insert_back(DataType value)
+{
+}
+
+bool DoublyLinkedList::remove(unsigned int index)
+{
+}
+
+bool DoublyLinkedList::remove_front()
+{
+}
+
+bool DoublyLinkedList::remove_back()
+{
+}
+
+bool DoublyLinkedList::replace(unsigned int index, DataType value)
+{
+}
+
+bool DoublyLinkedList::is_sorted_asc() const {
+}
+
+bool DoublyLinkedList::is_sorted_desc() const {
+}
+
+bool DoublyLinkedList::insert_sorted_asc(DataType val) {
+}
+
+bool DoublyLinkedList::insert_sorted_desc(DataType val) {
+}
+
+void DoublyLinkedList::sort_asc() {
+}
+    
+void DoublyLinkedList::sort_desc() {
+}
+
+
---
+++ a1_sequential_list.cpp
@@ -0,0 +1,129 @@
+#include "a1_sequential_list.hpp"
+#include <cstdlib>
+
+SequentialList::SequentialList(unsigned int cap)
+{
+	capacity_ = cap;
+	size_ = 0 ;
+	data_ = new DataType[cap];
+}
+
+SequentialList::~SequentialList()
+{
+	delete [] data_;
+	data_ = NULL;
+}
+
+unsigned int SequentialList::size() const
+{
+	return size_;
+}
+
+unsigned int SequentialList::capacity() const
+{
+	return capacity_;
+}
+
+bool SequentialList::empty() const
+{
+	if (size_ == 0) return true;
+	return false;
+}
+
+bool SequentialList::full() const
+{
+	if (size_ == capacity_) return true;
+	return false;
+}
+
+SequentialList::DataType SequentialList::select(unsigned int index) const
+{
+	if (index <= size_ && index >= 0){
+		return data_[index];
+	}
+	return data_[size_];
+}
+
+unsigned int SequentialList::search(DataType val) const
+{
+}
+
+void SequentialList::print() const
+{
+}
+
+bool SequentialList::insert(DataType val, unsigned int index)
+{
+}
+
+bool SequentialList::insert_front(DataType val)
+{
+	if (size_ == 0) {
+		data_[0] = val;
+		size_++;
+		return true;
+	}
+	else if (size_ == capacity_) {
+		return false;
+	}
+	else {
+		for (int i = size_; i >= 0 ; i--)  {
+			data_[i + 1] = data_[i];
+		}
+		data_[0] = val;
+		size_++;
+		return true;
+	}
+	
+}
+
+bool SequentialList::insert_back(DataType val)
+{
+	if (size_ < capacity_) {
+		data_[size_] = val;
+		size_++;
+		return true;
+	}
+	return false;
+		
+}
+
+bool SequentialList::remove(unsigned int index)
+{
+}
+
+bool SequentialList::remove_front()
+{
+}
+
+bool SequentialList::remove_back()
+{
+}
+
+bool SequentialList::replace(unsigned int index, DataType val)
+{
+}
+
+bool SequentialList::is_sorted_asc() const {
+
+}
+
+bool SequentialList::is_sorted_desc() const {
+
+}
+
+bool SequentialList::insert_sorted_asc(DataType val) {
+
+}
+
+bool SequentialList::insert_sorted_desc(DataType val) {
+
+}
+
+void SequentialList::sort_asc() {
+}
+    
+void SequentialList::sort_desc() {
+}
+
+
