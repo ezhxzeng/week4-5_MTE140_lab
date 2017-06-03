03967296ca5a85c16b3e18ca198a6265d373ed6e
Emily Zeng <zhixuan.zeng@gmail.com>
2017-06-03 15:16:48 -0400

    double linked list test 9 complete

--- a1_doubly_linked_list.cpp
+++ a1_doubly_linked_list.cpp
@@ -1,5 +1,5 @@
 #include "a1_doubly_linked_list.hpp"
-#include <cstdlib>
+#include <iostream>
 
 DoublyLinkedList::Node::Node(DataType data)
 {
@@ -74,10 +74,35 @@ unsigned int DoublyLinkedList::search(DataType value) const
 
 void DoublyLinkedList::print() const
 {
+	if(head_ == NULL) return;
+	std::cout << "(";
+	Node* node = head_;
+	while(node != NULL){
+		std::cout << "[" << node->value << "]" ;
+		node = node->next;
+		if (node!= NULL) std::cout << "-><-";
+	}
+	std::cout << "-> NULL" << std::endl;
 }
 
 DoublyLinkedList::Node* DoublyLinkedList::getNode(unsigned int index) const
 {
+	if (index > (size_ - 1)) return NULL;
+    else if (index == (size_ - 1)) return tail_;
+    else if (index > ((size_ - 1) / 2)) {
+        Node* select = tail_;
+        for (int i = 0; i < (size_ - 1 - index); i++) {
+            select = select->prev;
+        }
+        return select;
+    }
+    else {
+        Node* select = head_;
+        for (int i = 0; i <= index; i++) {
+            select = select->next;
+        }
+        return select;
+    }
 }
 
 bool DoublyLinkedList::insert(DataType value, unsigned int index)
@@ -154,6 +179,21 @@ bool DoublyLinkedList::insert_back(DataType value)
 
 bool DoublyLinkedList::remove(unsigned int index)
 {
+	if (index > (size_ - 1)) return false;
+    else if (index == (size_ - 1)){
+    	return remove_back();
+	}
+	else if (index == 0){
+		return remove_front(); 
+	}
+	else{
+		Node* temp = this->getNode(index);
+		temp->prev->next = temp->next;
+		temp->next->prev = temp->prev;
+		delete temp;
+		temp = NULL;
+		return true;
+	}
 }
 
 bool DoublyLinkedList::remove_front()
