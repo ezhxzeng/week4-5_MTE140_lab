28846db50200c5059dfb6b4fb1455fcecaeb79f5
Emily Zeng <zhixuan.zeng@gmail.com>
2017-06-07 22:26:45 -0400

    string bug fix

--- part3/a1_big_int.hpp
+++ part3/a1_big_int.hpp
@@ -2,6 +2,7 @@
 #define A1_BIG_INT_HPP
 
 #include "a1_doubly_linked_list2.hpp"
+#include <string>
 
 class BigInt {
 private:
@@ -14,14 +15,14 @@ public:
 	BigInt(bool B[], int size);
 	
 	//takes string input, creates cooresponding BigInt
-//	BigInt(std::string value);
-//	
-//	BigInt* add(BigInt* rhs); // computes “*this + *rhs”
-//	BigInt* sub(BigInt* rhs); // computes “*this - *rhs”
-//	BigInt* mul(BigInt* rhs); // computes “*this * *rhs” 
-//	
-//	BigInt* and_(BigInt* rhs); // computes bitwise AND of “*this & *rhs”
-//	BigInt* or_(BigInt* rhs); // computes bitwise OR of “*this | *rhs”
-//	BigInt* xor_(BigInt* rhs); // computes bitwise XOR of “*this ^ *rhs” 
+	BigInt(std::string value);
+	
+	BigInt* add(BigInt* rhs); // computes “*this + *rhs”
+	BigInt* sub(BigInt* rhs); // computes “*this - *rhs”
+	BigInt* mul(BigInt* rhs); // computes “*this * *rhs” 
+	
+	BigInt* and_(BigInt* rhs); // computes bitwise AND of “*this & *rhs”
+	BigInt* or_(BigInt* rhs); // computes bitwise OR of “*this | *rhs”
+	BigInt* xor_(BigInt* rhs); // computes bitwise XOR of “*this ^ *rhs” 
 };
 #endif 
